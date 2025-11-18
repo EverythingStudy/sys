73eb251b67f6086a25956efa2dcf57eed76827b0
chenly <759499199@qq.com>
2025-11-18 18:09:58 +0800

    BCrypt 密码编辑器

---
+++ staitech-user/src/main/java/com/koala/basic/user/StaitechUserApplication.java
@@ -0,0 +1,13 @@
+package com.koala.basic.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaitechUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaitechUserApplication.class, args);
+    }
+
+}
---
+++ staitech-user/src/test/java/com/koala/basic/user/StaitechUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koala.basic.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaitechUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
